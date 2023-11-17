1be2adf71d278bc9f512380540203bcdf363bfb6
JakeOh <jake.oh.lecture@gmail.com>
2023-11-17 11:20:54 +0900

    log4j2

--- lab05_jsp/src/main/java/com/itwill/jsp2/controller/HomeController.java
+++ lab05_jsp/src/main/java/com/itwill/jsp2/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.itwill.jsp2.controller;
 
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,6 +18,7 @@ import jakarta.servlet.http.HttpServletResponse;
 // "http://localhost:8081/jsp2" context root를 처리하는 서블릿
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -23,6 +27,7 @@ public class HomeController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 	        throws ServletException, IOException {
 		System.out.println("homeController::doGet()");
+		log.debug("doGet()");
 		
 		// 요청(request)를 뷰(view)로 전달.
 		request.getRequestDispatcher("/WEB-INF/home.jsp")
