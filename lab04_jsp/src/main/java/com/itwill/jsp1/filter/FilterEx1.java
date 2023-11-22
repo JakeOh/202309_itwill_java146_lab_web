5a1cc6bc34cea7be9725f0e4246eecf19674b102
JakeOh <jake.oh.lecture@gmail.com>
2023-11-22 15:23:36 +0900

    listener, filter

--- lab04_jsp/src/main/java/com/itwill/jsp1/filter/FilterEx1.java
+++ lab04_jsp/src/main/java/com/itwill/jsp1/filter/FilterEx1.java
@@ -28,6 +28,7 @@ public class FilterEx1 extends HttpFilter implements Filter {
 	@Override
     public void destroy() {
 		System.out.println("filterEx1::destroy() 호출");
+		// WAS가 종료될 때 생성된 필터를 소멸시킬 때 호출 - 리소스 해제
 	}
 
 	/**
@@ -35,12 +36,14 @@ public class FilterEx1 extends HttpFilter implements Filter {
 	 */
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		System.out.println("----- filterEx1: doFilter() 호출 전");
+		// 요청을 필터 체인(-> 서블릿)으로 전달하기 전에 실행할 코드들:
+	    System.out.println("----- filterEx1: doFilter() 호출 전");
 
-		// pass the request along the filter chain
+		// pass the request along the filter chain - 요청을 필터 체인으로 전달 -> 서블릿 실행
 		chain.doFilter(request, response);
 		
-		System.out.println("----- filterEx2: doFilter() 호출 후");
+		// 요청 처리가 끝난 후에 실행할 코드들
+		System.out.println("----- filterEx1: doFilter() 호출 후");
 	}
 
 	/**
@@ -49,6 +52,7 @@ public class FilterEx1 extends HttpFilter implements Filter {
 	@Override
 	public void init(FilterConfig fConfig) throws ServletException {
 		System.out.println("filterEx1::init() 호출");
+		// 필터가 생성된 후 초기화 작업 수행 - 필터 환경 설정(configuration)를 이용.
 	}
 
 }
