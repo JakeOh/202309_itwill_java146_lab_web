a3c91e5c124f9ac332ee6c3a2afa681664093c0c
JakeOh <jake.oh.lecture@gmail.com>
2023-11-27 12:12:46 +0900

    config spring legacy project

--- spring1/src/test/java/com/itwill/spring1/LogTest.java
+++ spring1/src/test/java/com/itwill/spring1/LogTest.java
@@ -2,16 +2,17 @@ package com.itwill.spring1;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class LogTest {
-    private static final Logger log = LoggerFactory.getLogger(LogTest.class);
+//    private static final Logger log = LoggerFactory.getLogger(LogTest.class);
     
     @Test
     public void test() {
         Assertions.assertNotNull(log);
-        log.info("test()");
+        log.info("test() - Lombok");
     }
 
 }
