219368d599a4aa8562682e6687e38f9c15c3c93c
JakeOh <jake.oh.lecture@gmail.com>
2023-11-29 10:25:15 +0900

    spring legacy project setting & junit test

--- spring2/src/test/java/com/itwill/spring2/datasource/HikariDataSourceTest.java
+++ spring2/src/test/java/com/itwill/spring2/datasource/HikariDataSourceTest.java
@@ -7,8 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.itwill.spring2.jdbc.JdbcTest;
 import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,13 +19,28 @@ import lombok.extern.slf4j.Slf4j;
 ) // 스프링 컨텍스트(환경 변수) 파일의 경로(이름).
 public class HikariDataSourceTest {
     
-    @Autowired
+    /*
+     * 의존성 주입(DI: dependency injection), 제어의 역전(IoC: Inversion of Control):
+     * 전통적인 자바 개발 방법에서는 객체를 사용하는 곳에서 객체를 생성하고, 그 기능을 이용함.
+     * 스프링 프레임워크에서는 스프링 컨터이너가 필요한 객체를 미리 메모리에 생성해 두고,
+     * 객체를 필요로 하는 곳에서는 변수 선언과 애너테이션만 사용하면
+     * 스프링 컨테이너가 생성/관리하고 있는 빈(bean)을 필요한 곳에 주입해 줌.
+     * application-context.xml 파일에 선언된 bean들을 스프링 컨터이너가 생성/관리함.
+     */
+    
+    @Autowired // 스프링 컨테이너가 생성/관리하는 빈(bean)을 변수에 자동 할당(주입).
     private HikariConfig config;
+    
+    @Autowired
+    private HikariDataSource ds;
     
     @Test
     public void test() {
         Assertions.assertNotNull(config);
         log.debug("config={}", config);
+        
+        Assertions.assertNotNull(ds);
+        log.debug("ds={}", ds);
     }
 
 }
