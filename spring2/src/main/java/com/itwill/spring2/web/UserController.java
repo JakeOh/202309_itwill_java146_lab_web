b20935f6854c75069664cfdabc122d0a40f0e0ae
JakeOh <jake.oh.lecture@gmail.com>
2023-12-06 15:24:31 +0900

    user sign-up, sing-in

--- spring2/src/main/java/com/itwill/spring2/web/UserController.java
+++ spring2/src/main/java/com/itwill/spring2/web/UserController.java
@@ -3,10 +3,13 @@ package com.itwill.spring2.web;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.itwill.spring2.dto.user.UserRegisterDto;
 import com.itwill.spring2.service.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -24,6 +27,21 @@ public class UserController {
     @GetMapping("/signup")
     public void signup() {
         log.debug("GET - signup()");
+    }
+    
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute UserRegisterDto dto) {
+        log.debug("POST - signup(dto={})", dto);
+        
+        // 회원가입 서비스 수행.
+        int result = userService.create(dto);
+        
+        return "redirect:/user/signin"; // 로그인(signin) 페이지로 이동(redirect)
+    }
+    
+    @GetMapping("/signin")
+    public void signin() {
+        log.debug("GET - signin()");
     }
     
     @GetMapping("/checkid")
