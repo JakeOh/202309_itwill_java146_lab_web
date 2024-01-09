525beb1e4c02320b9df56d0a51a8b89f17bfadb2
JakeOh <jake.oh.lecture@gmail.com>
2024-01-09 17:50:07 +0900

    JPA/Hibernate basic

---
+++ springboot1/src/main/java/com/itwill/springboot1/domain/Answer.java
@@ -0,0 +1,28 @@
+package com.itwill.springboot1.domain;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "answers")
+public class Answer {
+    
+    @Id
+    @GeneratedValue(generator = "answer_seq", strategy = GenerationType.SEQUENCE)
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    private Question question;
+    
+    @Basic(optional = false)
+    private String answer;
+
+}
--- springboot1/src/main/java/com/itwill/springboot1/domain/User.java
+++ springboot1/src/main/java/com/itwill/springboot1/domain/User.java
@@ -1,9 +1,15 @@
 package com.itwill.springboot1.domain;
 
+import java.math.BigDecimal;
+
 import org.hibernate.annotations.NaturalId;
 
 import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,14 +19,33 @@ import lombok.Data;
 @Data
 @Entity // JPA의 엔터티 클래스
 @Table(name = "USERS") // 데이터베이스의 테이블 이름을 설정.
-public class User {
+// 엔터티 클래스가 상속하는 상위 클래스에는 @MappedSuperclass 애너테이션을 사용해야 함.
+public class User extends BaseTimeEntity {
     
     @Id // Primary Key
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // MySQL의 autoincrement와 비슷한 기능.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    //-> Oracle의 generated as identity. MySQL의 autoincrement와 비슷한 기능.
     private Long id;
     
     @NaturalId // Unique 제약 조건.
     @Basic(optional = false) // 데이터베이스 기본 타입으로 매핑. Not Null 제약 조건.
     private String username;
+    
+    @Enumerated(EnumType.STRING)
+    //-> @Enumerated(EnumType.Ordinal) 기본값. 기본값인 경우에는 생략 가능.
+    //-> Ordinal: 숫자 타입 컬럼으로 매핑.
+    //-> String: 문자열 타입 컬럼으로 매핑.
+    private Gender gender;
+    
+    @Column(length = 1000) // length: 테이블 컬럼의 문자열 길이 설정. DDL 자동 실행할 때.
+    private String memo;
+    
+    @Column(name = "SAL", precision = 4, scale = 2)
+    //-> name: (엔터티 객체의 필드 이름과 테이블 컬럼 이름이 다를 때) 테이블의 컬럼 이름을 설정.
+    //-> precision, scale: DDL을 자동 실행할 때. 오라클에서는 Number(precision, scale) 매핑.
+    private BigDecimal salary;
+    
+    //@Embedded //-> @Embeddable 애너테이션을 사용한 객체를 포함. 생략 가능.
+    private Address address;
 
 }
