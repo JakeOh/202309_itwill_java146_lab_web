89232955a28208d5ec9d896617a10e9b4a1d7b80
JakeOh <jake.oh.lecture@gmail.com>
2024-01-22 12:20:34 +0900

    MemberRepository

--- springboot4/src/main/java/com/itwill/springboot4/domain/MemberRepository.java
+++ springboot4/src/main/java/com/itwill/springboot4/domain/MemberRepository.java
@@ -1,7 +1,14 @@
 package com.itwill.springboot4.domain;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // select m from Member m where m.username = ?
+    @EntityGraph(attributePaths = "roles")
+    Optional<Member> findByUsername(String username);
+    
 }
--- springboot4/src/test/java/com/itwill/springboot4/domain/MemberRepositoryTest.java
+++ springboot4/src/test/java/com/itwill/springboot4/domain/MemberRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.itwill.springboot4.domain;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +18,37 @@ public class MemberRepositoryTest {
     @Autowired private MemberRepository memberDao;
     @Autowired private PasswordEncoder passwordEncoder;
 
-    @Test
+//    @Test
     public void test() {
         Assertions.assertNotNull(memberDao);
         log.info("memberDao={}", memberDao);
         
         Assertions.assertNotNull(passwordEncoder);
         log.info("passwordEncoder={}", passwordEncoder);
+        
+        Member m = Member.builder()
+                .username("user2")
+                .password(passwordEncoder.encode("2222"))
+                .email("user2@itwill.com")
+                .build();
+        m.addRole(MemberRole.ADMIN);
+        
+        log.info("save 전: {}", m);
+        
+        memberDao.save(m); // insert 쿼리 실행.
+        log.info("save 후: {}", m);
+    }
+    
+//    @Test @Transactional
+    public void testFindAll() {
+        List<Member> list = memberDao.findAll();
+        list.forEach((x) -> log.info("{}, {}", x, x.getRoles()));
+    }
+    
+    @Test
+    public void testFindByUsername() {
+        Optional<Member> m = memberDao.findByUsername("admin");
+        log.info("{}, {}", m, m.get().getRoles());
     }
     
 }
